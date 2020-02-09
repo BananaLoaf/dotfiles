eef88cccf6046902142e009ec09d09c4eb299b5a
BananaLoaf <bananaloaf@protonmail.com>
2020-02-09 19:13:15 +0300

    Updated files

--- gnome-keyring-autostart.sh
+++ gnome-keyring-autostart.sh
@@ -4,6 +4,6 @@ if grep -q "pam_gnome_keyring.so" /etc/pam.d/sddm
 then
     echo "Already in autostart!"
 else
-    echo "auth		optional	pam_gnome_keyring.so             # gnome-keyring" | sudo tee -a /etc/pam.d/sddm
-	echo "session		optional	pam_gnome_keyring.so auto_start  # gnome-keyring" | sudo tee -a /etc/pam.d/sddm
+    echo "auth		optional	pam_gnome_keyring.so             # gnome-keyring" | sudo tee -a /etc/pam.d/sddm > /dev/null
+	echo "session		optional	pam_gnome_keyring.so auto_start  # gnome-keyring" | sudo tee -a /etc/pam.d/sddm > /dev/null
 fi
---
+++ sync_app_settings.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo_green ()
+{
+    local TEXT=$1
+    echo -e "\e[4;32m$TEXT\e[0m"
+}
+
+link ()
+{
+    local SOURCE=$1
+    local DEST=$2
+    echo "Linking $SOURCE -> $DEST"
+    mkdir -p $(dirname $DEST) # Make parent dir for destination
+    rm -r $DEST               # Remove existing destination
+    ln -s $SOURCE $DEST       # Link
+}
+
+if [[ $PWD == *dotfiles ]]
+then
+    cd apps
+
+    ################################################################
+    echo_green "Linking config files"
+    link $PWD/.config/fish $HOME/.config/fish
+    link $PWD/.config/fusuma $HOME/.config/fusuma
+    link $PWD/.config/latte $HOME/.config/latte
+    link $PWD/.local/share/konsole $HOME/.local/share/konsole
+    link $PWD/.local/share/user-places.xbel $HOME/.local/share/user-places.xbel
+    
+    ################################################################
+    # Change shell
+    NEW_SHELL=/usr/bin/fish
+    if ! [ $SHELL == $NEW_SHELL ]
+    then
+        echo_green "Changing default shell to fish"
+        chsh -s $NEW_SHELL
+    fi
+    
+    ################################################################
+    # For touchpad gestures
+    if groups $USER | grep -q -v input
+    then
+        echo_green "Adding $USER to input group"
+        sudo gpasswd -a $USER input        
+    fi
+
+    cd ..
+else
+	echo_green "You must be inside dotfiles directory!"
+fi
