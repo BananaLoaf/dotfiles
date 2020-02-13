6ca8eb82d7ce6b1f746978afca51573ef5f73302
BananaLoaf <bananaloaf@protonmail.com>
2020-02-13 16:02:43 +0300

    Updated files

--- kde/.config/autostart-scripts/sync_wallpapers.sh
+++ kde/.config/autostart-scripts/sync_wallpapers.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-HOME="/home/bark"
-
 rsync -rtvu --delete $HOME/Projects/dotfiles/wallpapers/ $HOME/Pictures/Wallpapers
 while inotifywait -qr --event modify,create,delete,move $HOME/Pictures/Wallpapers/
 do
