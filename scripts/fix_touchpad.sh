d4048ada70b13f045d33ca90e80fb0e77dbdba95
BananaLoaf <bananaloaf@protonmail.com>
2020-03-05 00:23:03 +0300

    Changed shortcuts and gestures

--- scripts/fix_touchpad.sh
+++ scripts/fix_touchpad.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 . ./scripts/funcs.sh --source-only
 
-echo_green "Add i8042.noloop to kernel parameters"
+sudo systemctl disable tlp  # Not sure
+echo_green "Install linux4.19"
+echo_green "Add i8042.nomux to kernel parameters"
 echo_green "Help: https://wiki.archlinux.org/index.php/Libinput#Touchpad_not_detected_at_all"
