86e25bbdb7d8b91cfa0499ac301bd5e642cef8fd
BananaLoaf <bananaloaf@protonmail.com>
2020-03-05 01:20:43 +0300

    Changed fish greeting

--- apps/.config/fish/functions/fish_greeting.fish
+++ apps/.config/fish/functions/fish_greeting.fish
@@ -1,9 +1,23 @@
 function fish_greeting
+    set GREETING ""
+    # Check for neofetch
     if command -v neofetch > /dev/null
-        if command -v lolcat > /dev/null
-            neofetch | lolcat
+        
+        # Check for buddhasay and roll the dice to be 1
+        if command -v buddhasay > /dev/null && [ 1 -eq (shuf -i 1-6 -n 1) ]
+            set GREETING "$GREETING buddhasay"
         else
-            neofetch
+            set GREETING "$GREETING neofetch"
         end
+
+    else
+        exit
+    end
+
+    # Color it, if possible, print
+    if command -v lolcat > /dev/null
+        fish -c "$GREETING" | lolcat
+    else
+        fish -c "$GREETING"
     end
 end 
--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -15,7 +15,7 @@ sudo pamac install $PACKAGES --no-confirm
 ################################################################
 echo_green "Installing AUR packages"
 
-PACKAGES_AUR="gcsfuse google-cloud-sdk tunnelbear google-chrome gitkraken btrbk sweet-theme-git ruby-fusuma \
+PACKAGES_AUR="buddhasay gcsfuse google-cloud-sdk tunnelbear google-chrome gitkraken btrbk sweet-theme-git ruby-fusuma \
 sweet-theme-git ruby-fusuma gotop ulauncher trezor-bridge-bin python37 dropbox tor-browser"
 yay -S --needed --noconfirm --batchinstall --noredownload --norebuild $PACKAGES_AUR 
 
