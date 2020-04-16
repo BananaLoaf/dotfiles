2ce65887105a8f7587cd73b22b525c0bc5aa94b7
BananaLoaf <bananaloaf@protonmail.com>
2020-04-16 08:35:46 +0300

    Updated package list

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -8,14 +8,14 @@ PACKAGES="gnome-keyring kgpg seahorse pix gimp lollypop telegram-desktop \
 deja-dup timeshift paper-icon-theme-git latte-dock neofetch lolcat gutenprint \
 ttf-opensans grub-customizer gparted net-tools zip unzip yay flameshot \
 fish gocryptfs etcher time tor torsocks inotify-tools transmission-qt \
-graphviz plasma-browser-integration firejail dbeaver wine"
+graphviz plasma-browser-integration firejail dbeaver wine strace qt5-tools"
 sudo pamac install $PACKAGES --no-confirm
 
 
 ################################################################
 echo_green "Installing AUR packages"
 
-PACKAGES_AUR="buddhasay gcsfuse google-cloud-sdk tunnelbear google-chrome gitkraken btrbk sweet-theme-git ruby-fusuma \
+PACKAGES_AUR="buddhasay gcsfuse google-cloud-sdk tunnelbear google-chrome gitkraken btrbk \
 sweet-theme-git ruby-fusuma gotop ulauncher trezor-bridge-bin python37 rocketchat-desktop networkmanager-l2tp"
 yay -S --needed --noconfirm --batchinstall --noredownload --norebuild $PACKAGES_AUR 
 PACKAGES_AUR_PROBLEMATIC="dropbox tor-browser"
@@ -26,7 +26,7 @@ echo_green "Installing SNAP packages"
 
 PACKAGES_SNAP_CLASSIC="clion pycharm-community sublime-text"
 PACKAGES_SNAP="spotify google-play-music-desktop-player whatsdesk \
-discord mailspring protonmail-desktop-unofficial"
+discord"
 
 for pkg in $PACKAGES_SNAP_CLASSIC
 do
