ca2a0ccc532ddbb5eb1398450014a9cac8e20db3
BananaLoaf <bananaloaf@protonmail.com>
2020-04-24 20:34:06 +0300

    Updated latte layout, active theme, added icons

--- sync_theme.sh
+++ sync_theme.sh
@@ -11,7 +11,10 @@ echo_green "Setting up KDE theming"
 link $PWD/.local/share/aurorae $HOME/.local/share/aurorae
 link $PWD/.local/share/color-schemes $HOME/.local/share/color-schemes
 link $PWD/.local/share/color-schemes $HOME/.kde4/apps/color-schemes
+link $PWD/.local/share/color-schemes $HOME/.kde4/share/apps/color-schemes
 link $PWD/.local/share/plasma/desktoptheme $HOME/.local/share/plasma/desktoptheme
 link $PWD/.local/share/plasma/look-and-feel $HOME/.local/share/plasma/look-and-feel
 link $PWD/.local/share/emoticons $HOME/.local/share/emoticons
 link $PWD/.icons $HOME/.icons
+link $PWD/.local/share/icons/PearBlue $HOME/.local/share/icons/PearBlue
+link $PWD/.local/share/icons/PearBlueDark $HOME/.local/share/icons/PearBlueDark
