bd81fd09d227006b54a6b1c9cbd1ff0d6e54161f
BananaLoaf <bananaloaf@protonmail.com>
2020-04-26 21:15:24 +0300

    Updated install and remove scripts

---
+++ scripts/install_packages.py
@@ -0,0 +1,138 @@
+from typing import List
+import os
+
+
+PACKAGES = {
+    "security": {
+        "gnome-keyring": "pacman", 
+        "kgpg": "pacman",
+        "seahorse": "pacman",
+        "tor": "pacman",
+        "torsocks": "pacman",
+        "firejail": "pacman",
+        "strace": "pacman",
+        "gocryptfs": "pacman",
+        "tunnelbear": "aur",
+        "networkmanager-l2tp": "aur",
+    },
+    "media": {
+        "pix": "pacman",
+        "gimp": "pacman",
+        "lollypop": "pacman",
+        "flameshot": "pacman",
+        "transmission-qt": "pacman",
+        "plasma-browser-integration": "pacman",
+        "google-chrome": "aur",
+        "sublime-text-3-imfix": "aur",
+        "tor-browser": "aur",
+        "dropbox": "aur",
+        "spotify": "aur",
+        "google-play-music-desktop-player": "snap",
+    },
+    "social": {
+        "telegram-desktop": "pacman",
+        "rocketchat-desktop": "aur",
+        "skype": "snap_classic",
+        "whatsdesk": "snap",
+        "discord": "snap",
+    },
+    "development": {
+        "dbeaver": "pacman",
+        "gcsfuse": "aur",
+        "google-cloud-sdk": "aur",
+        "gitkraken": "aur",
+        "python37": "aur",
+        "clion": "snap_classic",
+        "pycharm-community": "snap_classic",
+    },
+    "personalization": {
+        "paper-icon-theme-git": "pacman",
+        "latte-dock": "pacman",
+        "neofetch": "pacman",
+        "lolcat": "pacman",
+        "fish": "pacman",
+        "plasma5-applets-window-buttons": "aur",
+        "plasma5-applets-window-title": "aur",
+        "buddhasay": "aur",
+        "sweet-theme-git": "aur",
+        "ulauncher": "aur",
+    },
+    "backup": {
+        "deja-dup": "pacman",
+        "timeshift": "pacman",
+        "btrbk": "aur",
+    },
+    "touchpad-gestures": {
+        "ruby-fusuma": "aur.laptop",
+        "ruby-fusuma-plugin-keypress": "aur.laptop"
+    },
+    "utilities": {
+        "gutenprint": "pacman",
+        "ttf-opensans": "pacman",
+        "grub-customizer": "pacman",
+        "gparted": "pacman",
+        "net-tools": "pacman",
+        "zip": "pacman",
+        "unzip": "pacman",
+        "yay": "pacman",
+        "etcher": "pacman",
+        "time": "pacman",
+        "inotify-tools": "pacman",
+        "graphviz": "pacman",
+        "wine": "pacman",
+        "qt5-tools": "pacman",
+        "appmenu-gtk-module": "pacman",
+        "gotop": "aur",
+        "trezor-bridge-bin": "aur",
+    },
+}
+
+
+if __name__ == "__main__":
+	laptop = True
+
+	pacman_packages = []
+	aur_packages = []
+	snap_classic_packages = []
+	snap_packages = []
+
+	for category in PACKAGES:
+		for package, src in PACKAGES[category].items():
+			if src.startswith("pacman"):
+				if ".laptop" in src and laptop:
+					pacman_packages.append(package)
+				elif ".laptop" not in src:
+					pacman_packages.append(package)
+
+			elif src.startswith("aur"):
+				if ".laptop" in src and laptop:
+					aur_packages.append(package)
+				elif ".laptop" not in src:
+					aur_packages.append(package)
+
+			elif src.startswith("snap_classic"):
+				if ".laptop" in src and laptop:
+					snap_classic_packages.append(package)
+				elif ".laptop" not in src:
+					snap_classic_packages.append(package)
+
+			elif src.startswith("snap"):
+				if ".laptop" in src and laptop:
+					snap_packages.append(package)
+				elif ".laptop" not in src:
+					snap_packages.append(package)
+
+
+	print("Installing pacman packages")
+	os.system(f"sudo pacman -Sy --noconfirm --needed {' '.join(pacman_packages)}")
+
+	print("Installing aur packages")
+	os.system(f"yay -S --needed --noconfirm --batchinstall --noredownload --norebuild {' '.join(aur_packages)}")
+
+	print("Installing snap_classic packages")
+	for package in snap_classic_packages:
+		os.system(f"sudo snap install {package} --classic")
+
+	print("Installing snap packages")
+	for package in snap_packages:
+		os.system(f"sudo snap install {package}")
--- scripts/remove_packages.sh
+++ scripts/remove_packages.sh
@@ -7,16 +7,47 @@ echo_green "Removing packages"
 PACKAGES="firefox imagewriter gwenview kate konversation spectacle thunderbird yakuake kget zsh"
 for pkg in $PACKAGES
 do
-	sudo pamac remove $pkg
+	sudo pacman -R --noconfirm $pkg
 done
 
 ################################################################
 echo_green "Cleaning up"
+# firefox
 rm -r $HOME/.cache/mozilla
+rm -r $HOME/.mozilla
+
+# gwenview
+rm -r $HOME/.config/gwenviewrc
+rm -r $HOME/.config/org.kde.gwenviewrc
+rm -r $HOME/.kde/share/apps/gwenview
+rm -r $HOME/.kde/share/config/gwenviewrc
+rm -r $HOME/.kde4/share/apps/gwenview
+rm -r $HOME/.kde4/share/config/gwenviewrc
 rm -r $HOME/.local/share/gwenview
+rm -r $HOME/.local/share/org.kde.gwenview
+
+# kate
+rm -r $HOME/.config/katemetainfos
+rm -r $HOME/.config/katepartrc
+rm -r $HOME/.config/katerc
+rm -r $HOME/.config/kateschemarc
+rm -r $HOME/.config/katesyntaxhighlightingrc
+rm -r $HOME/.config/katevirc
 rm -r $HOME/.local/share/kate
-rm $HOME/.config/gwenviewrc
-rm $HOME/.config/katemetainfos
-rm $HOME/.config/katerc
-rm $HOME/.config/kateschemarc
-rm $HOME/.config/katevirc
+
+# konversation
+rm -r $HOME/.config/konversationrc
+rm -r $HOME/.kde/share/config/konversationrc
+rm -r $HOME/.kde4/share/config/konversationrc
+
+# spectacle
+rm -r $HOME/.config/spectaclerc
+
+# thunderbird
+rm -r $HOME/.cache/thunderbird
+rm -r $HOME/.thunderbird
+rm -r $HOME/.icedove
+
+# kget
+rm -r $HOME/.config/kgetrc
+rm -r $HOME/.config/kioslaverc
