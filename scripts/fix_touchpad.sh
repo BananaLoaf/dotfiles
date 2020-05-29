c77c31c2752d8760d2dc69c55264049d6b50920c
BananaLoaf <bananaloaf@protonmail.com>
2020-05-29 17:47:29 +0300

    Clean up

--- scripts/fix_touchpad.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-. ./scripts/funcs.sh --source-only
-
-sudo systemctl disable tlp  # Not sure
-echo_green "Install linux4.19"
-echo_green "Add i8042.nomux to kernel parameters"
-echo_green "Help: https://wiki.archlinux.org/index.php/Libinput#Touchpad_not_detected_at_all"
