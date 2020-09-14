1780b4abe330a077e4116fe7bd506486e83f2503
BananaLoaf <bananaloaf@protonmail.com>
2020-09-15 01:58:50 +0300

    Added fish functions

--- apps/.config/fish.syncpoint/config.fish
+++ apps/.config/fish.syncpoint/config.fish
@@ -1,3 +1,23 @@
 set EDITOR /bin/nano
 
+
+function confirm
+  while true
+    read -l -P 'Do you want to continue? [y/N] ' resp
+
+    switch $resp
+      case Y y
+        eval $argv
+        return
+      case '' N n
+        return
+    end
+  end
+end
+
+
 alias private-home="mkdir $HOME/PRVTH; firejail --noprofile --private=$HOME/PRVTH fish"
+
+alias logout="confirm qdbus org.kde.ksmserver /KSMServer logout 0 0 0"
+
+alias hibernate="confirm systemctl hibernate"
